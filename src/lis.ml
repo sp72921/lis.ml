84d072ab8ca53a81d1f8c97d3f93d01849e2d2a6
Soham Pednekar <sohampednekar2811@gmail.com>
2024-06-14 19:04:21 +0530

    Modified read_from_tokens function to ignore parentheses

--- src/lis.ml
+++ src/lis.ml
@@ -1,12 +1,8 @@
-type 'a number
-
 type atom =
   | Symbol of string
   | Number of int
   | Floating of float
 
-type 'a lst = 'a list
-type 'a expr = atom * 'a lst
 type 'a env = ('a * 'a) list
 
 let tokenize (chars: string) : string list =
@@ -22,28 +18,51 @@ let tokenize (chars: string) : string list =
   |> List.filter (fun c -> c <> "")
 
 let atom token =
-  List.map
-    (fun s ->
-      let int' = int_of_string_opt s in
-      match int' with
-        None ->
-         (match float_of_string_opt s with
-          | None -> Symbol s
-          | Some v -> Floating v)
-      | Some v -> Number v) token
+  let int' = int_of_string_opt token in
+  match int' with
+    None ->
+     (match float_of_string_opt token with
+      | None -> Symbol token
+      | Some v -> Floating v)
+  | Some v -> Number v
+
+let rec find_opt x lst =
+    match lst with
+    | [] -> None
+    | h :: t -> if x = h
+                then Some 0 else
+                  match find_opt x t with
+                  | None -> None
+                  | Some idx -> Some (idx + 1)
+
+let rec take n lst =
+  if n = 0 then [] else
+    match lst with
+    | [] -> []
+    | h :: t -> h :: take (n - 1) t
+
+let rec drop n l =
+  if n = 0 then l
+  else match l with
+       | [] -> []
+       | h :: t -> drop (n - 1) t
 
 let rec read_from_tokens tokens =
   match tokens with
-    [] -> failwith "Unexpected EOF"
-  (* | [x] -> TODO Incestigate to simplify this sphagetti code furthur *)
-  | h :: t -> if h = "(" then
-                read_from_tokens t
-              else if h = ")" then
-                failwith "Syntax Error: Unxpected )"
-              else atom t
+  | [] -> []
+  | h :: t ->
+     if String.equal h "(" then
+       (match find_opt "(" t with
+        | None ->
+           (match find_opt ")" t with
+            | None -> failwith "UNMATCHED `)'"
+            | Some v -> (take v t) :: read_from_tokens t
+           )
+        | Some n -> (take n t) :: read_from_tokens t)
+     else read_from_tokens t
 
 let parse p =
-  read_from_tokens (tokenize p)
+  read_from_tokens (tokenize p) |> List.map (List.map atom)
 
 let _ =
   let program = "(begin (define r 10) (* pi (* r r)))" in
